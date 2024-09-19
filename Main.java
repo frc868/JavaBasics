e8391dbc5e14aa31a3fd80c4cb01dd82a07f619c
Tsundoiii <91398247+Tsundoiii@users.noreply.github.com>
2024-09-19 02:11:23 -0400

    eepy

--- Main.java
+++ Main.java
@@ -1,5 +1,52 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        // Scanner scanner = new Scanner(System.in);
+
+        // String hello = "Hello World";
+        // int a = 3;
+        // boolean b = true;
+        /* Make a double variable named 'd' with the value 6.28 */
+
         System.out.println("Hello World!");
+
+        // if (a == 3) {
+        // System.out.println("a is 3! :D");
+        // } else if (a == 4) {
+        // System.out.println("a is 4! :>");
+        // } else {
+        // System.out.println("a is not 3 or 4! >:c");
+        // }
+
+        /* Counts up from 0 to 5 */
+        // int i = 0;
+        // while (i < 5) {
+        // System.out.println(i);
+        // i++;
+        // }
+
+        /* Same thing as the while loop above, expressed as a for loop */
+        // for (int j = 0; j < 5; j++) {
+        // System.out.println(j);
+        // }
+
+        /* Write while and for loops to count down from 5 to 0 here */
     }
+
+    // This is a void method because it doesn't return anything
+    // static void sayHi() {
+    // System.out.println("Hi! :3");
+    // }
+
+    // This method returns a value, it has a return type instead of 'void'
+    // static double add(double x, double y) {
+    // return x + y;
+    // }
+
+    /*
+     * Write a method that takes in two ints and prints out
+     * "Subtracting <second int> from <first int>", then returns the result of the
+     * subtraction
+     */
 }
